cf3680b6ebafb401f6a05028ef287a831e5a8974
sherviiinn <shervinshaebani1385@gmail.com>
2025-03-23 19:15:39 +0330

    person class updated

--- Person.java
+++ Person.java
@@ -5,4 +5,10 @@ public class Person {
         this.age = age;
         this.name = name;
     }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
